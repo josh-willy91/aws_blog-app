dbc0b6b89fff08b164b90ad7fd348da5f6cfc2cf
josh-willy91 <jwilly91812@yahoo.com>
2022-04-24 21:00:54 -0400

    Amplify configure

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+import { listPosts } from '../src/graphql/queries';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  async function fetchPosts() {
+    const postData = await API.graphql({
+      query: listPosts
+    })
+    setPosts(postData.data.listPosts.items)
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        My Posts
+      </h1>
+      {
+        posts.map((post, index) => (
+          <p>{post.title}</p>
+        ))
+      }
+    </div>
   )
 }
