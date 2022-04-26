cf2107fc6ebc660812bc7402e7a9cb5539a169c6
josh-willy91 <jwilly91812@yahoo.com>
2022-04-25 20:42:10 -0400

    solved configure issue

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,3 +1,9 @@
 import Amplify from "aws-amplify";
 import config from './src/aws-exports';
-Amplify.config(config);
+// Amplify.config(config);
+Amplify.configure({
+    ...config,
+    Analytics: {
+        disabled: true,
+    },
+});
