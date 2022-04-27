a0700398379dc9a5771a0ebc990e6edcd328f6bc
josh-willy91 <jwilly91812@yahoo.com>
2022-04-27 12:06:09 -0400

    added profile function with logic to login

---
+++ pages/profile.js
@@ -0,0 +1,36 @@
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+// if issues with Auth then you may need to append to the above import and add /legacy to the end of @aws-amplify/ui-react
+import { Auth } from "aws-amplify";
+import { useState, useEffect } from "react";
+
+function Profile() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        checkUser()
+    }, [])
+
+    async function checkUser() {
+        const user = await Auth.currentAuthenticatedUser()
+        setUser(user)
+    }
+
+
+    if (!user) return null;
+
+    return (
+        <div>
+            <h1 className='text-3xl font-semibold tracking-wide mt-6'>
+                Profile
+            </h1>
+            <h1 className='font-medium text-gray-500 my-2'>
+                {user.username}
+            </h1>
+            <p className='text-sm text-gray-500 mb-6'>
+                {user.attributes.email}
+            </p>
+        </div>
+    )
+}
+
+export default withAuthenticator(Profile);
