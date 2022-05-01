0828ef24b91bbe1ba0b80f8bc89dc041ef0b342b
josh-willy91 <jwilly91812@yahoo.com>
2022-05-01 14:41:04 -0500

    working my posts page

--- pages/my-posts.js
+++ pages/my-posts.js
@@ -1,11 +1,13 @@
 import { API, Auth } from 'aws-amplify';
 import { useEffect, useState } from 'react';
 import { postsByUsername } from '../src/graphql/queries';
+import Link from 'next/link';
 
 
 
 function MyPosts() {
-    const [posts, setPosts] = useState()
+    const [posts, setPosts] = useState([])
+
     useEffect(() => {
         fetchPosts()
 
@@ -17,7 +19,37 @@ function MyPosts() {
             query: postsByUsername,
             variables: { username }
         })
+
+        console.log(postData.data.postsByUsername.items, 'posts =========posts==============')
+        if(postData.data.postsByUsername.items = []) {
+            console.log('if conditional ============')
+            setPosts(['No Posts Currently'])
+        } else {
+            setPosts(postData.data.postsByUsername.items)
+        }
     }
+
+    return (
+        <div>
+            <h1 className='text-3xl font-semibold tracking-wide mt-6 mb-2p'>
+                My Posts
+            </h1>
+            {
+                posts.map((post, index) => (
+                    <Link key={index} href={`/posts/${post.id}`}>
+                        <div className='curser-pointer border-b border-gray-300 mt-8 pb-4'>
+                            <h2 className='text-xl font-semibold'>
+                                {post.title}
+                            </h2>
+                            <p className='text-gray-500 mt-2'>
+                                Author: {post.username}
+                            </p>
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
+    )
 }
 
 export default MyPosts;
