3198309db902dfa2112befad647d8ad2555695bc
josh-willy91 <jwilly91812@yahoo.com>
2022-05-21 11:07:34 -0500

    added storage for images

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "postImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
