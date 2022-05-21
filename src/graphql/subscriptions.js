cf43f1cbd638a3f135031e88385fe76714667821
josh-willy91 <jwilly91812@yahoo.com>
2022-05-21 11:58:43 -0500

    updated graphql schema

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,6 +1,30 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const newOnCreatePost = /* GraphQL */ `
+  subscription NewOnCreatePost {
+    newOnCreatePost {
+      id
+      title
+      content
+      username
+      coverImage
+      comments {
+        items {
+          id
+          message
+          postID
+          createdAt
+          updatedAt
+          createdBy
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const onCreatePost = /* GraphQL */ `
   subscription OnCreatePost($username: String) {
     onCreatePost(username: $username) {
@@ -9,6 +33,17 @@ export const onCreatePost = /* GraphQL */ `
       content
       username
       coverImage
+      comments {
+        items {
+          id
+          message
+          postID
+          createdAt
+          updatedAt
+          createdBy
+        }
+        nextToken
+      }
       createdAt
       updatedAt
     }
@@ -22,6 +57,17 @@ export const onUpdatePost = /* GraphQL */ `
       content
       username
       coverImage
+      comments {
+        items {
+          id
+          message
+          postID
+          createdAt
+          updatedAt
+          createdBy
+        }
+        nextToken
+      }
       createdAt
       updatedAt
     }
@@ -35,8 +81,91 @@ export const onDeletePost = /* GraphQL */ `
       content
       username
       coverImage
+      comments {
+        items {
+          id
+          message
+          postID
+          createdAt
+          updatedAt
+          createdBy
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+    }
+  }
+`;
+export const onCreateComment = /* GraphQL */ `
+  subscription OnCreateComment($createdBy: String) {
+    onCreateComment(createdBy: $createdBy) {
+      id
+      message
+      post {
+        id
+        title
+        content
+        username
+        coverImage
+        comments {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      postID
+      createdAt
+      updatedAt
+      createdBy
+    }
+  }
+`;
+export const onUpdateComment = /* GraphQL */ `
+  subscription OnUpdateComment($createdBy: String) {
+    onUpdateComment(createdBy: $createdBy) {
+      id
+      message
+      post {
+        id
+        title
+        content
+        username
+        coverImage
+        comments {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      postID
+      createdAt
+      updatedAt
+      createdBy
+    }
+  }
+`;
+export const onDeleteComment = /* GraphQL */ `
+  subscription OnDeleteComment($createdBy: String) {
+    onDeleteComment(createdBy: $createdBy) {
+      id
+      message
+      post {
+        id
+        title
+        content
+        username
+        coverImage
+        comments {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      postID
+      createdAt
+      updatedAt
+      createdBy
     }
   }
 `;
