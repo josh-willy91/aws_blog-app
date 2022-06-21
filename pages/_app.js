ab4bcf53a0f2d9552371d5e7625f38907599fd7a
josh-willy91 <jwilly91812@yahoo.com>
2022-06-20 21:05:18 -0500

    cheater

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import '../styles/globals.css';
 import '../configureAmplify';
 import Navbar from './components/Navbar'
 
+
 function MyApp({ Component, pageProps }) {
+  
   return (
     <div>
       <Navbar/>
