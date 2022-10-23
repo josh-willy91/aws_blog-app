9ff4e3e2c81ac3dced27a68a35ce708673408b82
josh-willy91 <jwilly91812@yahoo.com>
2022-10-23 18:57:57 -0500

    something

--- pages/profile.js
+++ pages/profile.js
@@ -3,6 +3,7 @@ import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { Auth } from "aws-amplify";
 import { useState, useEffect } from "react";
 
+
 function Profile() {
     const [user, setUser] = useState(null);
 
